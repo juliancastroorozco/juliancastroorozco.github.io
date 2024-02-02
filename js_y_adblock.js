da732e285791a964fdc043146caa605a5ec501ad
Julian Castro <36118800+juliancastroorozco@users.noreply.github.com>
2024-02-02 08:57:26 -0500

    fix

--- js_y_adblock.js
+++ js_y_adblock.js
@@ -64,7 +64,10 @@ setInterval(function (){
       if(channelSkip[channelName] > 0 ){
           timeleft -= channelSkip[channelName];
       }
-      if(timeleft < 5 && $('button[aria-label="Save to playlist"]').length){
+      if(!location.href.match('list=WL')){
+        return;
+      }
+      if(timeleft < 5 && $('button[aria-label="Save to playlist"]:not(.unwatched)').length){
           $('button[aria-label="Save to playlist"]').addClass('unwatched')
           $('button[aria-label="Save to playlist"]').click();
 
